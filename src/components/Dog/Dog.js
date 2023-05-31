6fb8225a9448a3bcbbb0238af6ed2458f70cf338
Sharie Trachsel <strachsel22@turing.edu>
2023-05-31 18:02:48 -0500

    Change dog to functional

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -3,30 +3,19 @@ import './Dog.css'
 import favoritedIcon from '../../assests/favorited-icon.png'
 import notFavoriteIcon from '../../assests/unfavorite-icon.png'
 
-class Dog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: this.props.id,
-      breed: this.props.breed,
-      imageUrl: this.props.imageUrl,
-      favorite: false
-    };
-  }
+const Dog = () => {
 
-  render(){
-  
-  const {dog} = this.props
- 
- 
-    return (
-      <section className="dog-card">
-        <img src={dog.imageUrl} alt={dog.breed} className="dog-image" />
-        {dog.favorite? <img src={favoritedIcon} onClick={() => this.props.updateDogFavorite(dog.id)}/> :
-                              <img src={notFavoriteIcon} onClick={() => this.props.updateDogFavorite(dog.id)}/> }
-      </section>
-    );
-    }
+  const { dog } = this.props
+
+
+  return (
+    <section className="dog-card">
+      <img src={dog.imageUrl} alt={dog.breed} className="dog-image" />
+      {dog.favorite ? <img src={favoritedIcon} onClick={() => this.props.updateDogFavorite(dog.id)} /> :
+                      <img src={notFavoriteIcon} onClick={() => this.props.updateDogFavorite(dog.id)} />}
+    </section>
+  );
 }
+
 
 export default Dog;
